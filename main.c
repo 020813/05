2d4d3ff5223e5c0cfec8cb002451ebce1258ac8a
020813 <113406823+020813@users.noreply.github.com>
2022-10-04 16:41:47 +0900

    02

--- main.c
+++ main.c
@@ -9,12 +9,10 @@ int main(int argc, char *argv[]) {
 	printf("정수 하나를 입력하시오 : ");
 	scanf("%d", &x);
 	
-	if (x>0)
-	  printf("양수입니다.\n");
-	else if (x<0) 
-	  printf("음수입니다.\n");
-	else
-	  printf("0입니다.\n");
+	if (x<0)
+	  x = -1*x;
+
+	printf("절댓값은 %i입니다.\n", x);
 	
 	return 0;
 }
