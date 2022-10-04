c1f13a3dbe628ac69de4349b931b6ba61003e5f4
020813 <113406823+020813@users.noreply.github.com>
2022-10-04 17:34:00 +0900

    05

--- main.c
+++ main.c
@@ -4,15 +4,23 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sum = 0;
-	int x;
-	int i;
+	int x, y;
+	char op;
+	int result;
 	
-	printf("input a number :");
-	scanf("%d", &x);
 	
-	for (i=0;i<=x;i++)
-	    sum = sum + i;
+	printf("enter the calculation : ");
+	scanf("%d %c %d", &x, &op, &y);
 	
-	printf("The result is %i.\n", sum);
+	if ( op == '+')
+	    result = x + y;
+	else if ( op == '-')
+	    result = x - y;
+	else if ( op == '*')
+	    result = x * y;
+	else 
+	    result = x / y;
+	
+	
+	printf("= %i\n", result);
 }
